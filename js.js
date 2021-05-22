702543a7b3efc3bdbb4705112fb2ac468d6d020c
dmitriy <dmitriydevelop@gmail.com>
2021-05-22 23:53:51 +0300

    js change

--- js.js
+++ js.js
@@ -4,8 +4,8 @@ var filesToCache = [
     'js.js',
     'icon/icon.svg'
 ];
-
-self.addEventListener('install', function(e) {
+console.log('test')
+window.addEventListener('install', function(e) {
     console.log('[ServiceWorker] Install');
     e.waitUntil(
         caches.open(cacheName).then(function(cache) {
@@ -14,3 +14,12 @@ self.addEventListener('install', function(e) {
         })
     );
 });
+
+$(document).bind( 'mousewheel', function (e) {
+  var nt = $(document.body).scrollTop()-(e.deltaY*e.deltaFactor*100);
+  e.preventDefault();
+  e.stopPropagation();
+  $(document.body).stop().animate( {
+    scrollTop : nt
+  } , 500 , 'easeInOutCubic' );
+} )
